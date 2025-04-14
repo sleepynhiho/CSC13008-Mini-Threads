3301a39732ada1237893f9644bc32ae70513a1af
sleepynhiho <nhiho.wkac@gmail.com>
2025-04-14 13:18:40 +0700

    fix: fix view home path

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ app.use(cookieParser());
 
 // Cài đặt EJS view engine và layouts
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'src', 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.use(ejsLayouts);
 
 // Cài đặt session
