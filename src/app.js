dd7ffa1b352ecc93cc74766f60113a20c94a41ec
sleepynhiho <nhiho.wkac@gmail.com>
2025-04-14 10:48:28 +0700

    fix: modify port

--- src/app.js
+++ src/app.js
@@ -80,6 +80,6 @@ app.use((req, res, next) => {
 });
 // Khởi động server
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
